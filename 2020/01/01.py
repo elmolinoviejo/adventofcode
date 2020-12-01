01822d8baede73297ffa5144f432f92182b4444e
Brett Babin <bmbabin@gmail.com>
2020-11-30 21:55:34 -0800

    2020 Day 1

---
+++ 2020/01/01.py
@@ -0,0 +1,30 @@
+def read_input(fn):
+    inputs = []
+    with open(fn) as fh:
+        for line in fh.readlines():
+            inputs.append(int(line.rstrip()))
+    return inputs
+
+
+def part1(inputs):
+    for n1 in inputs:
+        if 2020 - n1 in inputs:
+            return(n1 * (2020 - n1))
+
+
+def part2(inputs):
+    for n1 in inputs:
+        for n2 in inputs:
+            if n2 > 2020 - n1:
+                continue
+            elif 2020 - n1 - n2 in inputs:
+                return(n1 * n2 * (2020 - n1 - n2))
+
+
+inputs = read_input('01.txt')
+# Part 1
+print('Part 1:')
+print(part1(inputs))
+# Part 2
+print('Part 2:')
+print(part2(inputs))
