5bd7184ce74b763fe7b8293b489c548bdd5428e5
Brett Babin <bmbabin@gmail.com>
2022-12-04 21:31:21 -0800

    2022 d05

---
+++ 2022/05/05.py
@@ -0,0 +1,60 @@
+from re import findall
+from collections import deque
+
+
+def read_input(fn, ncols):
+    with open(fn, 'r') as fh:
+        crates, steps = fh.read().split('\n\n')
+
+    crates = crates.split('\n')
+    steps = steps.split('\n')
+
+    stacks = [deque() for x in range(ncols)]
+    for crate_line in crates[:-1]:
+        for c in range(ncols):
+            ind = (c * 4) + 1
+            lett = crate_line[ind]
+            if lett==' ':
+                continue
+            else:
+                stacks[c].append(lett)
+
+    moves = []
+    for line in steps:
+        move_nums = findall(r'\d+', line)
+        moves.append(tuple([int(x) for x in move_nums]))
+
+    return stacks, moves
+
+
+def move_crates(move, stacks, how='one'):
+    '''
+    how = 'one' or 'all'
+    '''
+    n_moves, fr, to = move
+    queue = deque()
+    for _ in range(n_moves):
+        lett = stacks[fr - 1].popleft()
+        queue.appendleft(lett)
+    if how=='one':
+        while queue:
+            stacks[to - 1].appendleft(queue.pop())
+    elif how=='all':
+        stacks[to - 1].extendleft(queue)
+
+    return stacks
+
+
+
+
+print('Part 1')
+stacks, moves = read_input('05.txt', 9)
+for m in moves:
+    stacks = move_crates(m, stacks, 'one')
+print(''.join([x[0] for x in stacks]))
+
+print('Part 2')
+stacks, moves = read_input('05.txt', 9)
+for m in moves:
+    stacks = move_crates(m, stacks, 'all')
+print(''.join([x[0] for x in stacks]))
